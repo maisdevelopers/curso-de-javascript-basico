cc2d6838338d276c3cb44246300441c9f56280b5
Plus Developers <developersmais@gmail.com>
2021-02-02 18:44:54 -0300

    Variaveis e tipos

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("Bem vindo ao curso de js");
+
+var nome = "Junior"; //String
+
+var idade = 34; // Number
+
+var dev = false; // Boolean
+
+var linguagens = ['javascript', 'Python', 'Java']; //Array
+
+console.log(linguagens[2])
